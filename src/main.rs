c8753923d5ba6231dd17e6d13cf13becb7768ebe
Magnus Balzer <magnus.balzer@gmail.com>
2022-03-10 16:06:52 +0100

    add example to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use tic_tac_toe::board::*;
+use tic_tac_toe::board::location::*;
 fn main() {
     println!("Welcome to Tic-Tac-Toe!");
+    let board = Board::new();
+    let board = board.place(Symbol::X, &Location::new(1,2).unwrap()).unwrap();
+    println!("{}", board);
 }
