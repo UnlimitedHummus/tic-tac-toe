3dce0373dc72ecdf0666b4456baf03731f3af4e2
Magnus Balzer <magnus.balzer@gmail.com>
2022-03-10 11:59:06 +0100

    add check to place method

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,12 @@ pub mod board {
     #[derive(Debug, PartialEq)]
     pub enum BoardError {
         InvalidLocation,
+        LocationTaken,
     }
 
-    #[derive(Debug,PartialEq, Eq, Clone, Copy)]
+    pub struct Location(pub u8, pub u8);
+
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
     pub enum Symbol {
         X,
         O,
@@ -13,23 +16,41 @@ pub mod board {
     pub struct Board {
         board: [[Option<Symbol>; 3]; 3],
     }
-    
+
     impl Board {
         pub fn new() -> Board {
             Board {
                 board: [[None; 3]; 3],
             }
         }
-    
-        pub fn place(self, _symbol: Symbol, location: (u8,u8)) -> Result<Self, BoardError>{
-            match location {
-                (x,y) if x <3 && y <3 => (),
-                (_,_) => return Err(BoardError::InvalidLocation)
-            };
-            todo!("implement the rest");
+
+        pub fn place(self, _symbol: Symbol, location: Location) -> Result<Self, BoardError> {
+            Board::valid_location(&location)?;
+
+            // check if place on board is free
+            if self.get_symbol(location) != None {
+                return Err(BoardError::LocationTaken);
+            }
+            todo!();
+        }
+
+        fn valid_location(location: &Location) -> Result<(), BoardError> {
+            match *location {
+                Location(x, y) if x < 3 && y < 3 => Ok(()),
+                Location(_, _) => return Err(BoardError::InvalidLocation),
+            }
+        }
+
+        fn get_symbol(&self, location: Location) -> Option<Symbol> {
+            *self
+                .board
+                .get(location.0 as usize)
+                .unwrap()
+                .clone()
+                .get(location.1 as usize)
+                .unwrap()
         }
     }
-    
 }
 
 #[cfg(test)]
@@ -38,7 +59,7 @@ mod tests {
     #[test]
     fn invalid_location() {
         let board = Board::new();
-        let res = board.place(Symbol::O, (3,2));
+        let res = board.place(Symbol::O, Location(3, 2));
         assert_eq!(res, Err(BoardError::InvalidLocation));
     }
 }
