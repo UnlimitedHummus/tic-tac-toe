b2403d5ed5d9cfaac878386792b343b1e5982f6b
Magnus Balzer <magnus.balzer@gmail.com>
2022-03-10 16:11:47 +0100

    added another symbol to example

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     println!("Welcome to Tic-Tac-Toe!");
     let board = Board::new();
     let board = board.place(Symbol::X, &Location::new(1,2).unwrap()).unwrap();
+    let board = board.place(Symbol::O, &Location::new(2,1).unwrap()).unwrap();
     println!("{}", board);
 }
