0062867a2224a48e49cad67402317ee185d86670
Magnus Balzer <magnus.balzer@gmail.com>
2022-03-12 22:37:33 +0100

    added player swap function

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -15,6 +15,10 @@ impl Game {
     fn new() -> Self {
         Game{board: Board::new(), active_player: Player::X}
     }
+
+    fn swap_active_player(&mut self) {
+        self.active_player = ! self.active_player;
+    }
 }
 
 #[cfg(test)]
@@ -24,5 +28,14 @@ mod tests {
     fn player_x_begins () {
         let game = Game::new();
         assert_eq!(game.active_player, Player::X)
+    }
+    #[test]
+    fn swap_players() {
+        let mut game = Game::new();
+        assert_eq!(game.active_player, Player::X);
+        game.swap_active_player();
+        assert_eq!(game.active_player, Player::O);
+        game.swap_active_player();
+        assert_eq!(game.active_player, Player::X);
     }
 }
--- src/game/player.rs
+++ src/game/player.rs
@@ -1,5 +1,17 @@
-#[derive(PartialEq, Debug)]
+use std::ops::Not;
+#[derive(PartialEq, Debug, Copy, Clone)]
 pub enum Player {
     X,
     O
+}
+
+impl Not for Player {
+    type Output=Self;
+
+    fn not(self) -> Self::Output {
+        match self{
+            Self::O => Self::X,
+            Self::X => Self::O,
+        }
+    }
 }
